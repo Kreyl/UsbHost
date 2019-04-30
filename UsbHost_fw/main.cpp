c5efb07113b4af197d171392bb5fea7bce61711e
kreyl <laystoll@yandex.ru>
2019-04-30 19:47:45 +0300

    Seems done

--- UsbHost_fw/main.cpp
+++ UsbHost_fw/main.cpp
@@ -22,6 +22,12 @@ void ITask();
 
 const PinOutput_t PillPwr {PILL_PWR_PIN};
 LedRGB_t Led { LED_R_PIN, LED_G_PIN, LED_B_PIN };
+
+Color_t Clr[24];
+uint8_t IBuf[72];
+void DrawFrame();
+
+static TmrKL_t TmrEverySecond {MS2ST(1000), evtIdEverySecond, tktPeriodic};
 #endif
 
 int main(void) {
@@ -48,21 +54,36 @@ int main(void) {
     PillPwr.SetHi();
 
     i2c2.Init();
+    i2c2.Resume();
 
     UsbCDC.Init();
     Clk.EnableCRS();
     Clk.SelectUSBClock_HSI48();
     UsbCDC.Connect();
 
+//    TmrEverySecond.StartOrRestart();
+
     // Main cycle
     ITask();
 }
+
+uint32_t n=0;
 
 __noreturn
 void ITask() {
     while(true) {
         EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
         switch(Msg.ID) {
+            case evtIdEverySecond:
+                Printf("s");
+                Clr[n] = clGreen;
+                if(n == 0) Clr[23] = clBlack;
+                else Clr[n-1] = clBlack;
+                n++;
+                if(n >= 24) n = 0;
+                DrawFrame();
+                break;
+
             case evtIdUsbNewCmd:
             case evtIdShellCmd:
                 Led.StartOrRestart(lsqUSBCmd); // After that, falling throug is intentional
@@ -107,6 +128,42 @@ void Resume() {
     i2c2.Resume();
 }
 
+#define BLOCK_CNT   12
+
+uint8_t i2c_color_msg[74];
+
+//void DrawLedBlock(uint8_t Addr,
+
+void DrawLedBlock(uint8_t Addr, Color_t *PClr) {
+    // Send colors data
+    i2c_color_msg[0] = 0x01;
+    for (int led_num=0; led_num < BLOCK_CNT; ++led_num) {
+        int led_offset = led_num * 3;
+        i2c_color_msg[0+led_offset + 1] = PClr[led_num].R;
+        i2c_color_msg[1+led_offset + 1] = PClr[led_num].G;
+        i2c_color_msg[2+led_offset + 1] = PClr[led_num].B;
+    }
+    i2c_color_msg[37] = 0x26;
+    memset(i2c_color_msg + 38, 0x01, 36);
+    i2c2.Write(Addr, i2c_color_msg, 74);
+
+    // Send FrameOut
+    uint8_t data[4];
+    data[0] = 0x25;
+    data[1] = 0x01;
+    data[2] = 0x00;
+    data[3] = 0x01;
+
+    i2c2.Write(Addr, data, 2);
+    i2c2.Write(Addr, data+2, 2);
+}
+
+void DrawFrame() {
+    DrawLedBlock(0x3C, &Clr[0]);
+    DrawLedBlock(0x3F, &Clr[12]);
+}
+
+
 #if 1 // ================= Command processing ====================
 void OnCmd(Shell_t *PShell) {
 	Cmd_t *PCmd = &PShell->Cmd;
@@ -116,6 +173,27 @@ void OnCmd(Shell_t *PShell) {
     if(PCmd->NameIs("Ping")) {
         PShell->Ack(retvOk);
     }
+
+    else if(PCmd->NameIs("tst")) {
+        DrawFrame();
+        PShell->Ack(retvOk);
+    }
+
+
+    else if(PCmd->NameIs("set")) {
+        if(PCmd->GetArray<uint8_t>(IBuf, 72) == retvOk) {
+            // Map buf on colors
+            uint8_t *p = IBuf;
+            for(int i=0; i<24; i++) {
+                Clr[i].FromRGB(p[0], p[1], p[2]);
+                p += 3;
+            }
+            DrawFrame();
+            PShell->Ack(retvOk);
+        }
+        else PShell->Ack(retvBadValue);
+    }
+
 
     else if(PCmd->NameIs("Scan")) {
         i2c2.ScanBus(PShell);
